8d6e213820611579d7eb7ec52a016c851e270816
chan-00 <a82316@naver.com>
2022-03-26 14:13:48 +0900

    StringClass.h 작성
    문자열을 담기 위한 클래스의 헤더 파일, char 배열이나 포인터 방식의 변수가 아닌 String이라는 이름의 객체를 만들어 문자열을 담기 위한 클래스의 헤더 파일입니다.

---
+++ StringClass.h
@@ -0,0 +1,19 @@
+﻿#ifndef __SClass__
+#define __SClass__
+
+class String {
+private:
+	char* str;
+	int len;
+public:
+	String();
+	String(const char* cstr);
+	String(const String& s);
+	String operator=(const String& s);
+	void SetString(const char* cstr);
+	void SetString(const String& s);
+	char* GetString() const;
+	~String();
+};
+
+#endif
