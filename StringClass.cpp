27e3831b0030e1768a1a13c0caa8ffc36bb34fdf
chan-00 <a82316@naver.com>
2022-03-28 19:50:05 +0900

    StringClass.cpp 작성
    StringClass.h 헤더 파일에서 선언되었던 클래스의 함수들을 정의하는 파일입니다.

---
+++ StringClass.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+#include<cstring>
+#include "StringClass.h"
+
+using namespace std;
+
+String::String() {
+	str = NULL;
+	len = 0;
+}
+
+String::String(const char* cstr) {
+	len = strlen(cstr) + 1;
+	str = new char[len];
+	strcpy(str, cstr);
+}
+
+String::String(const String& s) {
+	len = strlen(s.str) + 1;
+	str = new char[len];
+	strcpy(str, s.str);
+}
+
+String String::operator=(const String& s) {
+	if (str != NULL)
+		delete[]str;
+
+	len = strlen(s.str) + 1;
+	str = new char[len];
+	strcpy(str, s.str);
+	String newStr(str);
+
+	return newStr;
+}
+
+void String::SetString(const char* cstr) {
+	if (str != NULL)
+		delete[]str;
+
+	len = strlen(cstr) + 1;
+	str = new char[len];
+	strcpy(str, cstr);
+}
+
+void String::SetString(const String& s) {
+	if (str != NULL)
+		delete[]str;
+
+	len = strlen(s.str) + 1;
+	str = new char[len];
+	strcpy(str, s.str);
+}
+
+char* String::GetString() const {
+	return str;
+}
+
+String::~String() {
+	if (str != NULL)
+		delete[]str;
+}
