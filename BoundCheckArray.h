d0735649f76307dbe73fb07ec62c73bbf90baa38
chan-00 <a82316@naver.com>
2022-03-29 16:21:37 +0900

    BoundCheckArray.h 헤더 파일 작성
    프로그램의 동작 과정 중(계좌관리 프로그램) 계좌들을 관리하는 객체인 AccArray 클래스를 선언하는 헤더 파일입니다.

---
+++ BoundCheckArray.h
@@ -0,0 +1,42 @@
+﻿#ifndef __AccountArr__
+#define __AccountArr__
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+template <typename T>
+class AccArray {
+private:
+	T* arr;
+	int arrlen;
+
+	AccArray(const T& arr);
+	AccArray& operator=(const T& arr);
+public:
+	AccArray(int len = 50) : arrlen(len) {
+		arr = new T[len];
+	}
+	T& operator[](int i) {
+		if (i < 0 && i >= arrlen) {
+			cout << "배열의 범위를 벗어납니다." << endl;
+			exit(1);
+		}
+		return arr[i];
+	}
+	T& operator[](int i) const {
+		if (i < 0 && i >= arrlen) {
+			cout << "배열의 범위를 벗어납니다." << endl;
+			exit(1);
+		}
+		return arr[i];
+	}
+	int GetArrLen() const {
+		return arrlen;
+	}
+	~AccArray() {
+		delete[]arr;
+	}
+};
+
+#endif
